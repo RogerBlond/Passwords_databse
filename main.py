e3f44731b35cb7a3e47fb850381ab05e41b10b31
RogerBlond <rfbombard@gmail.com>
2023-02-10 10:30:51 +0100

    adding break lines

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def psswd_encode(text):
 
 def core():
     while True:       
-            mode = input('Would you like to add a new password or view existing ones? (add, view, press q to quit)').lower()
+            mode = input('Would you like to add a new password or view existing ones?\
+                        (add, view, press q to quit)').lower()
             if mode == 'q':
                 break
 
@@ -45,7 +46,9 @@ def core():
                 continue
 
 def generate():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', \
+                'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',\
+                'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '@','#', '$', '%', '^','&', '*','(', ')','/','-', '+']
 
